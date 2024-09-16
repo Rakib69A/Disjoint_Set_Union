f729c1141c8c472c6344e029a222512f0186014f
Rakib <rakibhasan75089@gmail.com>
2024-09-16 22:11:00 +0600

    dsu union by size is done

--- dsu.cpp
+++ dsu.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 const int N = 1e5+5;
 int parent[N];
+int group_size[N];
 void dsu_initialization(int n){
     for(int i=0;i<n;i++){
         parent[i] = -1;
+        group_size[i] = 1;
     }
-    parent[1] = 2;
-    parent[2] = 3;
-    parent[5] = 6;
-    parent[6] = 7;
 }
 int dsu_find(int node){
     if(parent[node] == -1)
@@ -23,12 +21,27 @@ void dsu_union(int node1, int node2){
     int leaderB = dsu_find(node2);
     parent[leaderA] = leaderB;
 }
+void dsu_union_by_size(int node1, int node2){
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+    if(group_size[leaderA] > group_size[leaderB]){
+        parent[leaderB] = leaderA;
+        group_size[leaderA]+=group_size[leaderB];
+    }
+    else{
+        parent[leaderA] = leaderB;
+        group_size[leaderB]+=group_size[leaderA];
+    }
+}
 int main()
 {
-    dsu_initialization(8);
+    dsu_initialization(7);
+    dsu_union_by_size(1,2);
+    dsu_union_by_size(2,3);
+    dsu_union_by_size(4,5);
+    dsu_union_by_size(5,6);
     cout<<dsu_find(1)<<endl;
-    dsu_union(1,5);
-    cout<<dsu_find(1)<<endl;
-    cout<<dsu_find(5)<<endl;
+    cout<<dsu_find(4)<<endl;
+
     return 0;
 }
