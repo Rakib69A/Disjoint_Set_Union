c169f8ba13d52995526968741f05130981b31e3a
Rakib <rakibhasan75089@gmail.com>
2024-09-16 20:30:34 +0600

    dsu_find function included just :

--- dsu.cpp
+++ dsu.cpp
@@ -1,9 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int N = 1e5+5;
+int parent[N];
+void dsu_initialization(int n){
+    for(int i=0;i<n;i++){
+        parent[i] = -1;
+    }
+    parent[1] = 3;
+    parent[2] = 1;
+}
+int find(int node){
+    if(parent[node] == -1)
+        return node;
+    int leader = find(parent[node]);
+    parent[node] = leader;
+    return leader;
+}
 int main()
 {
-    int node;
-    cin>>node;
-    cout<<node;
+    dsu_initialization(4);
+    cout<<find(0)<<endl;
+    cout<<find(1)<<endl;
+    cout<<find(2)<<endl;
+    cout<<find(3)<<endl;
     return 0;
 }
