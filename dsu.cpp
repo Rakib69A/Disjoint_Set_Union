f54b79af6554e2db8cd455fd9946ce79f4376183
Rakib <rakibhasan75089@gmail.com>
2024-09-16 20:51:18 +0600

    dsu_union added

--- dsu.cpp
+++ dsu.cpp
@@ -6,22 +6,29 @@ void dsu_initialization(int n){
     for(int i=0;i<n;i++){
         parent[i] = -1;
     }
-    parent[1] = 3;
-    parent[2] = 1;
+    parent[1] = 2;
+    parent[2] = 3;
+    parent[5] = 6;
+    parent[6] = 7;
 }
-int find(int node){
+int dsu_find(int node){
     if(parent[node] == -1)
         return node;
-    int leader = find(parent[node]);
+    int leader = dsu_find(parent[node]);
     parent[node] = leader;
     return leader;
 }
+void dsu_union(int node1, int node2){
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+    parent[leaderA] = leaderB;
+}
 int main()
 {
-    dsu_initialization(4);
-    cout<<find(0)<<endl;
-    cout<<find(1)<<endl;
-    cout<<find(2)<<endl;
-    cout<<find(3)<<endl;
+    dsu_initialization(8);
+    cout<<dsu_find(1)<<endl;
+    dsu_union(1,5);
+    cout<<dsu_find(1)<<endl;
+    cout<<dsu_find(5)<<endl;
     return 0;
 }
